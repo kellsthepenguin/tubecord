ad98a2d4035aa19d81cdcad97cd104aa6e8dd732
Kim Jioo <jwkim101201@gmail.com>
2021-10-25 07:55:08 +0900

    feat: edit ping message

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,7 +2,7 @@ import { Message } from 'discord.js'
 import Client from '../classes/Client'
 
 export default function (client: Client, msg: Message) {
-  msg.channel.send('pong! (' + client.ws.ping + 'ms)')
+  msg.channel.send(':ping_pong: Pong! **' + client.ws.ping + 'ms**')
 }
 
 export const aliases = ['ping', '핑', 'pong']
